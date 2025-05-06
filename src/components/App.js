07e1ff35b44721d9b818fedb605acd6ce075ad22
SinghVanshdeep <vansh.bains57@gmail.com>
2025-05-06 23:44:39 +0100

    Quote text align fix

--- src/components/App.js
+++ src/components/App.js
@@ -23,7 +23,7 @@ function App(){
     <div id="main">
       <div id="container">
         <p className="h3 playfair-display-text mt-4">Random Quote Generator</p>
-        <p className="h2 playfair-display-quote mt-2">"{quote}"</p>
+        <p id="quote" className="h2 playfair-display-quote mt-2">"{quote}"</p>
         <p className="h6 playfair-display-text">{loading}</p>
         <button className="btn mb-4" onClick={handleClick}>GET A QUOTE</button>
       </div>
